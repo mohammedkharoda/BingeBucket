911d131ade89ed6795046a31ce898b963e62372d
Mohammed Kharoda <kharodawalam@gmail.com>
2024-09-01 13:01:44 +0530

    fix 🥲

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,12 +1,16 @@
+import { Suspense } from "react";
+
 import MovieShowcase from "@/components/movieComponents/MovieShowcase";
 import SortedMovie from "@/components/seriesComponents/SortedMovies";
 import TrendingMoviesBanner from "@/components/movieComponents/TrendingMoviesBanner";
 export default function Movies() {
   return (
-    <div className="lg:px-[64px] lg:py-[112px] px-[20px] py-[64px]">
-      <MovieShowcase />
-      <TrendingMoviesBanner />
-      <SortedMovie />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="lg:px-[64px] lg:py-[112px] px-[20px] py-[64px]">
+        <MovieShowcase />
+        <TrendingMoviesBanner />
+        <SortedMovie />
+      </div>
+    </Suspense>
   );
 }
